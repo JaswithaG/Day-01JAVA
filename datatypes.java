d71209d9ac8f89151a91662d8989af4fb95f2495
JaswithaG <gummijaswitha@gmail.com>
2024-12-02 14:40:23 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class datatypes{
+    public static void main(String args[]){
+        int a=9;
+        char b='J';
+        float c=3.14f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
+}
