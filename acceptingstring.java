3f7fc4ac620024a9dfef9f3d84de1d140e3186c2
JaswithaG <gummijaswitha@gmail.com>
2024-12-02 16:19:05 +0530

    Add files via upload

---
+++ acceptingstring.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class acceptingstring{
+    public static void main(String args[]){
+        //Accepting a string
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String b=sc.nextLine();
+        System.out.print(a);
+        System.out.print(b);
+    }
+}
